99d165fda870155c82ed2f75cfb3e527807f0337
Lydia Yoo <bb9e.bb8.c3po.r2d2@gmail.com>
2021-10-22 15:50:09 +0900

    init commit

---
+++ src/main/java/com/crispycode/myjmt/MyjmtApplication.java
@@ -0,0 +1,13 @@
+package com.crispycode.myjmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyjmtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyjmtApplication.class, args);
+    }
+
+}
